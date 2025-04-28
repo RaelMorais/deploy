dcded537ec5a909e351d3d3da0414b784b3df58d
RaelMorais <israelsantanamorais@outlook.com>
2025-04-27 22:30:16 -0300

    teste

--- WebServer/settings.py
+++ WebServer/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',  # Comment this out or conditionally disable for API views
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
